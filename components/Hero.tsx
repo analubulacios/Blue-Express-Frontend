50355b62dbb02100aef987344b1cff38ed3bcfaa
analubulacios <analubula87@gmail.com>
2024-02-26 18:03:19 -0300

    chore: name app

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
           <div className="blur-[106px] h-60 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
       </div>
       <h1 className={urbanistic.className + ' text-gray-900 dark:text-white font-bold mb-6 text-3xl md:text-4xl xl:text-5xl'}>
-        Create a <span className='bg-gradient-to-b from-transparent from-40% to-purple-300 to-40%'>short link</span> and QR code easily.
+        Create a <span className='bg-gradient-to-b from-transparent from-40% to-purple-300 to-40%'>short link</span> easily.
       </h1>
     </>
   )

5d983a9af473aca7983605f40cbd4733adc0b788
analubulacios <analubula87@gmail.com>
2024-02-27 10:09:05 -0300

    chore: change name

--- components/URLGrid.tsx
+++ components/URLGrid.tsx
@@ -2,18 +2,21 @@ import { getAllShortURLs } from '@/lib/actions'
 import { URLShorted } from '@/types/url.model'
 import React from 'react'
 import CardURL from './CardURL'
+import { getServerSession } from 'next-auth'
 
 const URLGrid = async () => {
   const shortURLs: URLShorted[] = await getAllShortURLs()
-
+  const session = await getServerSession()
   const totalClicks = shortURLs.reduce((acc, curr) => acc + (curr.clicks ?? 0), 0)
 
   return (
     <div>
+   {session && 
       <div className='flex gap-4 justify-center mb-8'>
-        <span className='text-sm'>Total clicks: <span className='font-semibold'>{totalClicks}</span></span>
-        <span className='text-sm'>Total shorts URLs: <span className='font-semibold'>{shortURLs.length}</span></span>
-      </div>
+      <span className='text-sm'>Total clicks: <span className='font-semibold'>{totalClicks}</span></span>
+      <span className='text-sm'>Total shorts URLs: <span className='font-semibold'>{shortURLs.length}</span></span>
+    </div>
+   }
       <section className='flex flex-wrap w-full gap-4 justify-center'>
         { shortURLs.map((url, k) => <CardURL key={k} url={url} />) }
       </section>
